3cd22170273b3b0f3f9dd1c220c73aa856f56646
Ishara Musippaththu <129735900+ishrdev@users.noreply.github.com>
2024-03-31 22:21:44 +0000

    feat: add projects section and swiperjs integration

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,3 +7,38 @@ const animateMenu = () => {
 }
 
 menu.addEventListener('click', animateMenu)
+
+
+let swiper = new Swiper('.project_slide-container', {
+    // Optional parameters
+    slidesPerView: 3,
+    direction: 'horizontal',
+    loop: true,
+    centerSlide: "true",
+    centeredSlidesBounds: "true",
+    fade: "true",
+    shortSwipes: false,
+    longSwipes: false,
+
+    // pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints:{
+        600: {
+            slidesPerView: 4,
+        },
+        968: {
+            slidesPerView: 4,
+        },
+    },
+});
