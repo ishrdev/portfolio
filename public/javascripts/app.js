4d4d42e2e7148904e2ee504da432671da86f4a67
Ishara Musippaththu <129735900+ishrdev@users.noreply.github.com>
2024-04-01 16:30:59 +0000

    feat: add proficiencies and animated skill bars

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,6 +8,28 @@ const animateMenu = () => {
 
 menu.addEventListener('click', animateMenu)
 
+// animation on scroll 
+const observer = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry => {
+        console.log(entry);
+        console.log(entry.isIntersecting);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('progress-animation');
+        } else {
+            entry.target.classList.remove('progress-animation');
+        }
+
+        });
+    });
+    
+    //Get multiple elements instead of a single one using "querySelectorAll"
+    const squares = document.querySelectorAll('.progress');
+    //console.log(squares);
+    // Loop over the elements and add each one to the observer
+    squares.forEach((element) => observer.observe(element));
+
+
 
 let swiper = new Swiper('.project_slide-container', {
     // Optional parameters
