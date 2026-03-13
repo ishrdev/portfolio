f34164aead4e02e718ce215efe84438236d29b92
Ishara Musippaththu <129735900+ishrdev@users.noreply.github.com>
2026-03-13 23:11:56 +0100

    feat: add socials

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import githubLogo from './assets/Github.svg'
+import linkedInLogo from './assets/Linkedin.svg'
 import DarkVeil from './assets/DarkVeil'
 
 import './App.css'
@@ -22,6 +24,17 @@ function App() {
             <div className='title md:text-6xl -mb-8 text-4xl'>ISHARA MUSIPPATHTHU</div>
             <p className='text-xl'>UX/UI Designer • Frontend Developer</p>
             <p className='text-2xl'>Designing intuitive digital experiences through user-centered design — and bringing them to life with code.</p>
+          </div>
+          <div>
+            <p className='text-xl'>Let's connect!</p>
+            <div className='w-full flex align-middle justify-center'>
+              <a href="https://www.linkedin.com/in/ishara-musippaththu" target="_blank">
+                <img src={linkedInLogo} className="logo" alt="LinkedIn logo" />
+              </a>
+              <a href="https://vite.dev" target="_blank">
+                <img src={githubLogo} className="logo" alt="Github logo" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
