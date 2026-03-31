3a80e04279071ee5e03ba65ff30063b38758feb3
Ishara Musippaththu <129735900+ishrdev@users.noreply.github.com>
2026-03-31 17:39:52 +0200

    feat: implement router and move content to home component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,166 @@
+import DarkVeil from '../assets/DarkVeil'
+import MagicBento from '../components/MagicBento'
+import logos from '../assets/logos.tsx'
+
+import '../App.css'
+
+export default function Home() {
+  const designSkills = [
+    {name: 'Figma', logo: logos.figma},
+    {name: 'Adobe Photoshop', logo: logos.photoshop},
+    {name: 'Adobe Illustrator', logo: logos.illustrator},
+    {name: 'Affinity', logo: logos.affinity}
+  ];
+
+  const frontendSkills = [
+    { name: 'TypeScript', logo: logos.ts },
+    { name: 'JavaScript', logo: logos.js },
+    { name: 'Vue.js', logo: logos.vue },
+    { name: 'Vuetify', logo: logos.vuetify },
+    { name: 'React', logo: logos.react },
+    { name: 'TailwindCSS', logo: logos.tw },
+    { name: 'HTML5', logo: logos.html },
+    { name: 'CSS', logo: logos.css },
+  ];
+
+  const backendSkills = [
+    {name: 'Python', logo: logos.py},
+    {name: 'Java', logo: logos.jv},
+    {name: 'FastAPI', logo: logos.fastapi},
+    {name: 'LangChain', logo: logos.langchain}
+  ]
+
+  const dbSkills = [
+    {name: 'PostgreSQL', logo: logos.postgres},
+    {name: 'MySQL', logo: logos.mysql},
+    {name: 'MongoDB', logo: logos.mongodb},
+  ]
+
+  const cadSkills = [
+    {name: 'Autodesk Maya', logo: logos.maya},
+    {name: 'Blender', logo: logos.blender},
+    {name: 'Rhino 3D', logo: logos.rhino},
+  ]
+
+  const toolsSkills = [
+    {name: 'Git', logo: logos.git},
+    {name: 'Bitbucket', logo: logos.bitbucket},
+    {name: 'Jira', logo: logos.jira},
+  ]
+  
+  return (
+    <div className='grid gap-2'>
+      <div className="block absolute top-0 left-0 w-full h-screen -z-1 row-start-1 col-start-1">
+        <DarkVeil
+          hueShift={0}
+          noiseIntensity={0}
+          scanlineIntensity={0}
+          speed={1}
+          scanlineFrequency={1.9}
+          warpAmount={0}
+        />
+      </div>
+      <section id='home' className='row-start-1 col-start-1 justify-around'>
+        {/*<Navbar/>*/}
+        <div className='flex flex-col h-svh content-center justify-evenly justify-items-center px-8'>
+          <div className='flex flex-col gap-16'>
+            <div className='title md:text-6xl -mb-8 text-4xl'>ISHARA MUSIPPATHTHU</div>
+            <p className='text-xl'>UX/UI Designer • Frontend Developer</p>
+            <p className='text-2xl'>Designing intuitive digital experiences through user-centered design — and bringing them to life with code.</p>
+          </div>
+          <div>
+            <p className='text-xl'>Let's connect!</p>
+            <div className='w-full flex align-middle justify-center'>
+              <a href="https://www.linkedin.com/in/ishara-musippaththu" target="_blank">
+                <img src={logos.linkedIn} className="logo" alt="LinkedIn logo" />
+              </a>
+              <a href="https://vite.dev" target="_blank">
+                <img src={logos.github} className="logo" alt="Github logo" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id='work'className='flex justify-center'>
+        <div className='flex flex-col flexstart'>
+          <div className="title text-2xl text-left mb-8">SELECTED WORK</div>
+          <MagicBento 
+            textAutoHide={true}
+            enableStars={false}
+            enableSpotlight
+            enableBorderGlow={true}
+            enableTilt={false}
+            enableMagnetism={false}
+            clickEffect
+            spotlightRadius={410}
+            particleCount={12}
+            glowColor="132, 0, 255"
+            disableAnimations={false}
+          />
+          <div className="text-xl mt-12">See more projects</div>
+        </div>
+      </section>
+      <section id='skills' className='flex justify-center'>
+        <div className='flex flex-col content-center justify-center'>
+          <div className='title text-2xl text-left mb-8'>SKILLS</div>
+          <div>
+            <div className='flex row-start-1 items-center mb-8'>
+              <div className='title text-xl text-left min-w-45'>Design</div>
+              <div className='grid grid-cols-2 lg:grid-cols-4 w-full gap-6'>
+                {designSkills.map(skill => (
+                  <div className='flex items-center'>
+                  <img src={skill.logo} alt={skill.name} className='logo'/>{skill.name}</div>
+                ))}
+              </div>
+            </div>
+            <div className='flex row-start-2 items-start'>
+              <div className='title text-xl text-left min-w-45'>Frontend</div>
+              <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 w-full'>
+                {frontendSkills.map(skill => (
+                  <div className='flex items-center'>
+                  <img src={skill.logo} alt={skill.name} className='logo'/>{skill.name}</div>
+                ))}
+              </div>
+            </div>
+            <div className='flex row-start-3 items-center'>
+              <div className='title text-xl text-left min-w-45'>Backend</div>
+              <div className='grid grid-cols-2 lg:grid-cols-4 w-full gap-6'>
+                {backendSkills.map(skill => (
+                  <div className='flex items-center'>
+                  <img src={skill.logo} alt={skill.name} className='logo'/>{skill.name}</div>
+                ))}
+              </div>
+            </div>
+            <div className='flex row-start-4 items-center'>
+              <div className='title text-xl text-left min-w-45'>Database</div>
+              <div className='grid grid-cols-2 lg:grid-cols-4 w-full gap-6 justify-evenly'>
+                {dbSkills.map(skill => (
+                  <div className='flex items-center'>
+                  <img src={skill.logo} alt={skill.name} className='logo'/>{skill.name}</div>
+                ))}
+              </div>
+            </div>
+            <div className='flex row-start-5 items-center'>
+              <div className='title text-xl text-left min-w-45'>CAD Modelling</div>
+              <div className='grid grid-cols-2 lg:grid-cols-4 w-full gap-6 justify-evenly'>
+                {cadSkills.map(skill => (
+                  <div className='flex items-center'>
+                  <img src={skill.logo} alt={skill.name} className='logo'/>{skill.name}</div>
+                ))}
+              </div>
+            </div>
+            <div className='flex row-start-6 items-center'>
+              <div className='title text-xl text-left min-w-45'>Tools</div>
+              <div className='grid grid-cols-2 lg:grid-cols-4 w-full gap-6 justify-evenly'>
+                {toolsSkills.map(skill => (
+                  <div className='flex items-center'>
+                  <img src={skill.logo} alt={skill.name} className='logo'/>{skill.name}</div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
