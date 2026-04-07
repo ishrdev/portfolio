e7085054889eda361bfa2477a92d34c99272542c
Ishara Musippaththu <129735900+ishrdev@users.noreply.github.com>
2026-04-07 23:01:45 +0200

    feat: add work page

---
+++ src/pages/Work.tsx
@@ -0,0 +1,12 @@
+function Work() {
+  return(
+    <>
+      <div className="flex flex-col w-full justify-center items-center-safe p-8 gap-16">
+        <h1 className='title md:text-6xl text-4xl text-left w-full'>MY WORK</h1>
+        <p className='text-left w-full'>Browse through my projects and get to know my working process!</p>
+      </div>
+    </>
+  )
+}
+
+export default Work
