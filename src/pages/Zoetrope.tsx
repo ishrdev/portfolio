eb78d44db74b35787ca55901923e174930a72b75
Ishara Musippaththu <129735900+ishrdev@users.noreply.github.com>
2026-04-14 23:50:43 +0200

    feat: add page for project 3D Zoetrope

---
+++ src/pages/Zoetrope.tsx
@@ -0,0 +1,11 @@
+function Zoetrope() {
+  return (
+    <>
+    <div className="flex flex-col w-full justify-center items-start px-32 py-8 gap-16 text-left">
+      <div className="title text-4xl">3D ZOETROPE</div>
+    </div>
+    </>
+  )
+}
+
+export default Zoetrope
