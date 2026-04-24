7e956711fdfb777625b4bb1282646ce3a65c55e0
Ishara Musippaththu <129735900+ishrdev@users.noreply.github.com>
2026-04-24 20:26:20 +0200

    feat: add banner to FairGig

--- src/pages/Fairgig.tsx
+++ src/pages/Fairgig.tsx
@@ -1,12 +1,17 @@
 import TechTag from "../components/TechTag"
 import logos from "../assets/logos"
+import fairgigBanner from "../assets/images/fairgig/fairgig_banner.png"
 import InfoArch from "../assets/images/fairgig/informationsarchitektur.png"
 
 function FairGig() {
   return (
     <>
+    <div className="relative h-[50%] w-full overflow-hidden">
+      <img src={fairgigBanner} alt="FairGig Banner" className="w-full h-full object-cover" />
+      <div className="absolute bottom-0 left-0 w-full h-8 bg-linear-to-t from-black to-transparent" />
+    </div>
     <div className="flex flex-col w-full justify-center items-start px-32 py-8 gap-16 text-left">
-      <div className="title text-4xl">FairGig</div>
+      <div className="title text-4xl">FAIRGIG</div>
       <div className="flex w-full justify-between gap-8">
         <div className="flex flex-col w-2/3 gap-4">
           <p className="title">Used Technologies</p>
@@ -43,7 +48,7 @@ function FairGig() {
         income uncertainty, and platform structures, combining research, concept development, and interface design.
       </p>
       <div className="flex flex-col gap-4">
-        <h2 className="title">Project Goal</h2>
+        <h2 className="title text-lg">Project Goal</h2>
         <p>FairGig is a mobile application designed to bring transparency to the gig economy. It enables users to track 
           their earnings and expenses, compare their income anonymously with peers, and gain insights through data-driven 
           forecasts. Additionally, the app integrates community features to foster exchange, support, and collective 
@@ -51,7 +56,7 @@ function FairGig() {
         </p>
       </div>
       <div className="flex flex-col gap-4 w-full">
-        <h2 className="title">Information Architecture</h2>
+        <h2 className="title text-lg">Information Architecture</h2>
         <div className="flex justify-center">
           <img src={InfoArch} alt="Workflow" className="rounded-2xl" />
         </div>
